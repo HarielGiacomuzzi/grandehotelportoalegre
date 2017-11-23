0356ca5fb3d42202f4c86acc87259dead5e0c937
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2017-11-23 20:44:17 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = module.exports = express();
+
+const http = require('http').Server(app);
+
+const port = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+
+
+app.use(express.static('./public/'));
+app.use('/img', express.static('./public/images/'));
+app.use('/img', express.static('./public/galeria/'));
+// application -------------------------------------------------------------
+app.get('/galeria', function(req, res) {
+    res.sendfile(`${__dirname}/public/galeria.html`);
+});
+
+app.get('/sobre', function(req, res) {
+    res.sendfile(`${__dirname}/public/sobre.html`);
+});
+
+app.get('*', (req, res) => {
+  res.send(`${__dirname}/public/index.html`);
+});
+
+http.listen(port, () => {
+  console.log(`App server running on port ${port}!`);
+});
